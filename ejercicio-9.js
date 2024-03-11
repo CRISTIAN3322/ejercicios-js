7f1f78210dcb6a7f904db873ab2dbabc2353f0d5
CCPOVEDA3322 <ccpoveda.programador@gmail.com>
2024-03-11 04:57:41 -0500

    primer commit

---
+++ ejercicio-9.js
@@ -0,0 +1,13 @@
+// Ejercicio N° 9: crear variables con la sentencia var.
+
+var numero;
+
+numero = 7;
+
+console.log(numero); 
+console.log(typeof(numero));
+
+numero = '7';
+
+console.log(numero); 
+console.log(typeof(numero));
